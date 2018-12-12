4d74a4fd75f4d24f759f0ce6361f0b0609b1d056
Alex Patterson <ajonp@alexs-mbp.lan>
2018-12-12 08:20:26 -0500

    show that the index.js works

--- src/index.js
+++ src/index.js
@@ -1,7 +1,8 @@
 // JS Goes here - ES6 supported
-
-import "./css/main.css";
-import "../node_modules/@ionic/core/css/ionic.bundle.css";
-import "../node_modules/@ionic/core/dist/ionic.js";
 // Say hello
 console.log("😺 Come say Hi (or Meow) at https://ajonp.com, maybe we can help!");
+
+import "./css/main.css";
+// import "../node_modules/@ionic/core/css/ionic.bundle.css";
+import "../node_modules/@ionic/core/dist/ionic.js";
+
