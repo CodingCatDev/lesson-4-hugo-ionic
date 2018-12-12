0b95a14dbde34642188a8cba3f61ee9c5365ce49
Alex Patterson <ajonp@alexs-mbp.lan>
2018-12-12 07:59:01 -0500

    add webpack

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // JS Goes here - ES6 supported
 
 import "./css/main.css";
-
+import "../node_modules/@ionic/core/css/ionic.bundle.css";
+import "../node_modules/@ionic/core/dist/ionic.js";
 // Say hello
-console.log("🦊 Hello! Edit me in src/index.js");
+console.log("😺 Come say Hi (or Meow) at https://ajonp.com, maybe we can help!");
